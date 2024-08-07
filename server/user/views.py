b05d1ed282e4d90e5a2ad28a5be85633ae1d49da
Dipak Kukadiya <dipak@anques.com>
2024-08-07 13:30:11 +0530

    Task

---
+++ server/user/views.py
@@ -0,0 +1,149 @@
+from django.contrib.auth import authenticate
+from django.conf import settings
+from django.middleware import csrf
+from rest_framework import exceptions as rest_exceptions, response, decorators as rest_decorators, permissions as rest_permissions
+from rest_framework_simplejwt import tokens, views as jwt_views, serializers as jwt_serializers, exceptions as jwt_exceptions
+from user import serializers, models
+from web3 import Web3
+
+INFURA_URL = "https://mainnet.infura.io/v3/2dacea50c39c45e5a4d9bdb25a133454"
+web3 = Web3(Web3.HTTPProvider(INFURA_URL))
+
+def get_user_tokens(user):
+    refresh = tokens.RefreshToken.for_user(user)
+    return {
+        "refresh_token": str(refresh),
+        "access_token": str(refresh.access_token)
+    }
+
+
+@rest_decorators.api_view(["POST"])
+@rest_decorators.permission_classes([])
+def loginView(request):
+    serializer = serializers.LoginSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    email = serializer.validated_data["email"]
+    password = serializer.validated_data["password"]
+
+    user = authenticate(email=email, password=password)
+
+    if user is not None:
+        tokens = get_user_tokens(user)
+        res = response.Response()
+        res.set_cookie(
+            key=settings.SIMPLE_JWT['AUTH_COOKIE'],
+            value=tokens["access_token"],
+            expires=settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'],
+            secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
+            httponly=settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
+            samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
+        )
+
+        res.set_cookie(
+            key=settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'],
+            value=tokens["refresh_token"],
+            expires=settings.SIMPLE_JWT['REFRESH_TOKEN_LIFETIME'],
+            secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
+            httponly=settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
+            samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
+        )
+
+        res.data = tokens
+        res["X-CSRFToken"] = csrf.get_token(request)
+        return res
+    raise rest_exceptions.AuthenticationFailed(
+        "Email or Password is incorrect!")
+
+
+@rest_decorators.api_view(["POST"])
+@rest_decorators.permission_classes([])
+def registerView(request):
+    serializer = serializers.RegistrationSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    user = serializer.save()
+
+    if user is not None:
+        return response.Response("Registered!")
+    return rest_exceptions.AuthenticationFailed("Invalid credentials!")
+
+
+@rest_decorators.api_view(['POST'])
+@rest_decorators.permission_classes([rest_permissions.IsAuthenticated])
+def logoutView(request):
+    try:
+        refreshToken = request.COOKIES.get(
+            settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'])
+        token = tokens.RefreshToken(refreshToken)
+        token.blacklist()
+
+        res = response.Response()
+        res.delete_cookie(settings.SIMPLE_JWT['AUTH_COOKIE'])
+        res.delete_cookie(settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'])
+        res.delete_cookie("X-CSRFToken")
+        res.delete_cookie("csrftoken")
+        res["X-CSRFToken"]=None
+        
+        return res
+    except:
+        raise rest_exceptions.ParseError("Invalid token")
+
+
+class CookieTokenRefreshSerializer(jwt_serializers.TokenRefreshSerializer):
+    refresh = None
+
+    def validate(self, attrs):
+        attrs['refresh'] = self.context['request'].COOKIES.get('refresh')
+        if attrs['refresh']:
+            return super().validate(attrs)
+        else:
+            raise jwt_exceptions.InvalidToken(
+                'No valid token found in cookie \'refresh\'')
+
+
+class CookieTokenRefreshView(jwt_views.TokenRefreshView):
+    serializer_class = CookieTokenRefreshSerializer
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        if response.data.get("refresh"):
+            response.set_cookie(
+                key=settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'],
+                value=response.data['refresh'],
+                expires=settings.SIMPLE_JWT['REFRESH_TOKEN_LIFETIME'],
+                secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
+                httponly=settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
+                samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
+            )
+
+            del response.data["refresh"]
+        response["X-CSRFToken"] = request.COOKIES.get("csrftoken")
+        return super().finalize_response(request, response, *args, **kwargs)
+
+
+@rest_decorators.api_view(["GET"])
+@rest_decorators.permission_classes([rest_permissions.IsAuthenticated])
+def user(request):
+    try:
+        user = models.User.objects.get(id=request.user.id)
+    except models.User.DoesNotExist:
+        return response.Response(status_code=404)
+
+    eth_address = user.ethereum_wallet_address
+
+    # Check if the Ethereum address is valid
+    if not Web3.is_address(eth_address):
+        balance_eth = 0
+    else:
+        # Get the balance of the Ethereum wallet address
+        balance_wei = web3.eth.get_balance(eth_address)
+        balance_eth = Web3.from_wei(balance_wei, 'ether')
+
+    # Serialize the user data
+    serializer = serializers.UserSerializer(user)
+    user_data = serializer.data
+
+    # Add the Ethereum balance to the response
+    user_data['eth_balance'] = balance_eth
+
+    return response.Response(user_data)
